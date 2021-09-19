a5b6570d978bc2b9d6dcdebf777ecbb6d15de30e
Stevewrote <87452458+Stevewrote@users.noreply.github.com>
2021-09-19 19:48:38 +1000

    Update and rename Practical Task 3 to Practical Task 3.js

---
+++ Assignment 2./Week1/Practical Task 3.js
@@ -0,0 +1,23 @@
+<button onclick="myFunction()">Confirm you are not a Bandit</button>
+
+
+function myFunction() {
+  alert("Bandits Detected!");
+}
+
+
+var studentMarks = [['Maths', 80], ['English', 77], ['Japanese', 88], ['Geography', 95], ['History', 68]];
+
+var Avgmarks = 0;
+
+for (var i=0; i < studentMarks.length; i++) {
+        Avgmarks += studentMarks[i][1];
+        var avg = (Avgmarks/studentMarks.length);
+  {
+
+console.log("Total Score: " + 
+console.log("Average grade: " + (Avgmarks)/studentMarks.length);
+console.log
+
+         
+  }
