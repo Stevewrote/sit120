e4947932067625cfed9784163378a67ce4886c0a
Stevewrote <87452458+Stevewrote@users.noreply.github.com>
2021-09-26 00:29:29 +1000

    Create Vue.js

---
+++ Assignment 2./Week4/Vue.js
@@ -0,0 +1,18 @@
+var Drendering = new Vue({
+    el: '#Drendering',
+    data: {
+      message: "Today's date is " + new Date().toLocaleString()
+    }
+  })
+
+  var vforexample = new Vue({
+    el: '#vforexample',
+    data: {
+      items: [
+        { message: 'Geraldine' },
+        { message: 'Harold' },
+        { message: 'Gladys' },
+        { message: 'Arthur' }
+      ]
+    }
+  })
