f08c53211d39d61aea0e07eb376ed32cc295beb8
Stevewrote <87452458+Stevewrote@users.noreply.github.com>
2021-09-26 00:37:30 +1000

    Delete Week7Part1&2index.js

--- Assignment 2./Week7/Week7Part1&2index.js
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue'
-import PopupWindow from './components/PopupWindow'
-import App from "./App.vue"
-
-const app = createApp(App)
-
-app.component('PopupWindow', PopupWindow) // global registration - can be used anywhere
-
-app.mount('#app')
